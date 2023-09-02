50c182451f7f049c1cb100b311d1f58a9fb22cc1
Amrutapatil07 <amrupatil17@gmail.com>
2023-09-02 16:39:32 +0530

    Employee -- admin

---
+++ src/test/java/pkg1/Employee.java
@@ -0,0 +1,12 @@
+package pkg1;
+
+public class Employee {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int id;
+		System.out.println("in Employee class");
+	}
+
+}
