0328dccb9fedc79fe3cde87a04e16ec90756cafc
Amrutapatil07 <143537342+Amrutapatil07@users.noreply.github.com>
2023-09-02 17:44:21 +0530

    Update Address.java

--- src/test/java/pkg1/Address.java
+++ src/test/java/pkg1/Address.java
@@ -5,6 +5,12 @@ public class Address {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		System.out.println("Hi");
+		System.out.println("Hi");
+		System.out.println("Hi");
+		System.out.println("Hi");
+		System.out.println("Hi");
+		
 	}
 
 }
